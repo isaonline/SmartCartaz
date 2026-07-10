6f85182127c50efe2be19c0759c265aed15bef5b
isaonline <isapaesnascimento@gmail.com>
2026-07-10 16:01:46 -0300

    Base do projeto em JS e mudanças estéticas

---
+++ script.js
@@ -0,0 +1,48 @@
+const formContainer = document.querySelector('#dados-form')
+
+// Estado que vai ler cada informação colocada nos inputs
+const state = {}
+
+// Listener para alterações realizadas nos inputs
+formContainer.addEventListener('input', (event) => {
+    const field = event.target.dataset.field;
+
+    if (!field) return;
+
+    state[field] = event.target.value;
+
+    console.log(state);
+})
+
+const preview = {
+    nome: document.querySelector('#cartaz-nome'),
+    idade: document.querySelector('#cartaz-idade'),
+    raca: document.querySelector('#cartaz-raca'),
+    cor: document.querySelector('#cartaz-cor'),
+    descricao: document.querySelector('#cartaz-desc'),
+    local: document.querySelector('#cartaz-texto-local'),
+    data: document.querySelector('#cartaz-texto-data'),
+    telefone: document.querySelector('#cartaz-tel-escolhido'),
+    recompensa: document.querySelector('#cartaz-recompensa'),
+    foto: document.querySelector('#cartaz-imagem')
+}
+
+const placeholders = {
+    nome: "Nome da pessoa aqui",
+    idade: "Idade da pessoa aqui",
+    descricao: "A descrição do desaparecimento com detalhes aparecerá aqui, não esqueça de escrever características físicas marcantes da pessoa",
+    local: "Bairro, Cidade - Estado",
+    data: "00/00/0000",
+    telefone: "(11) 99999-9999",
+    recompensa: "",
+    foto: ""   
+}
+
+// Função para renderizar as mudanças na prévia
+function renderPreview() {
+for (const campo in preview) {
+        if (state[campo]) {
+            preview[campo].textContent = state[campo] || placeholders[campo];
+        }
+    }
+}
